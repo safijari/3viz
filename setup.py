f410dd0b1e9c2ef4fe92a88058b0e1b0e8078770
Jariullah Safi <safijari@isu.edu>
2020-01-23 15:01:23 -0800

    Change name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='3viz',
+    name='threeviz',
     version="0.0.1",
     packages=find_packages(),
     install_requires=['tornado', 'numpy', 'tiny_tf', 'msgpack'])
