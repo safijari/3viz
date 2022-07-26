2bf4896e75d3732f614715de4ae19a2cfe744dde
Jariullah <safijari@isu.edu>
2022-07-26 09:39:03 -0400

    0.0.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="threeviz",
-    version="0.0.6",
+    version="0.0.7",
     packages=find_packages(),
     install_requires=["tornado", "numpy", "tiny_tf", "msgpack"],
 )
