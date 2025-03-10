c840d444860dcc08e3c4cb4fbca4bec63e25d53b
Jariullah Safi <safijari@isu.edu>
2025-03-10 15:36:47 -0500

    bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="threeviz",
-    version="0.0.7",
+    version="0.0.8",
     packages=find_packages(),
     install_requires=["tornado", "numpy", "tiny_tf", "msgpack"],
 )
